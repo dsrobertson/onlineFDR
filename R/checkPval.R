7c3d6085f7bee86a56cf1f81aae814472c460b22
lathanliou <lltk2015@mymail.pomona.edu>
2020-12-07 16:54:28 +0000

    fix checkpval

--- R/checkPval.R
+++ R/checkPval.R
@@ -1,16 +1,29 @@
 checkPval <- function(d) {
     
-    if (sum(is.na(d$pval)) > 0) {
-        warning("Your data contains missing values. Missing values were omitted.")
-        d <- na.omit(d)
+    if (is.data.frame(d)) {
+        if (any(is.na(d$pval))) {
+            warning("Your data contains missing p-values. Missing p-values were omitted.")
+            d <- na.omit(d)
+        }
+        
+        if (!(is.numeric(d$pval))) {
+            stop("The vector of p-values contain at least one non-numeric element.")
+        } else if (any(d$pval > 1 | d$pval < 0)) {
+            stop("All p-values must be between 0 and 1.")
+        } 
+        
+    } else if (is.vector(d)) {
+        
+        if (any(is.na(d))) {
+            warning("Your data contains missing p-values. Missing p-values were omitted.")
+            d <- d[!is.na(d)]
+        }
+        if (!(is.numeric(d))) {
+            stop("The vector of p-values contain at least one non-numeric element.")
+        } else if (any(d > 1 | d < 0)) {
+            stop("All p-values must be between 0 and 1.")
+        }
     }
-    
-    if (!(is.numeric(d$pval))) {
-        stop("The vector of p-values contain at least one non-numeric element.")
-    } else if (any(d$pval > 1 | d$pval < 0)) {
-        stop("All p-values must be between 0 and 1.")
-    }
-    
     return(d)
 }
 TRUE
--- tests/testthat/test_checkPval.R
+++ tests/testthat/test_checkPval.R
@@ -1,5 +1,5 @@
 test_that("Check error messages", {
-    expect_warning(checkPval(c(0.1,NA)), "Missing p-values were ignored")
+    expect_warning(checkPval(c(0.1,NA)), "Your data contains missing p-values. Missing p-values were omitted.")
     
     expect_error(checkPval(c(0.1,'A')),
                  "The vector of p-values contain at least one non-numeric element.")
